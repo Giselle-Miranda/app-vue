1669699fd6777663ba35c0afcc9606c463408576
Giselle <gi.miranda2424@gmail.com>
2025-09-24 11:54:27 -0300

    Arquivos iniciais do projeto Vue

---
+++ src/components/Carrossel.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/img/logo.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src=".../assets/img/2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src=".../assets/img/3.jpg" class="d-block w-100" alt="...">
+      
+    </div>
+  </div>
+</div>
+    
+
+</template>
+
+<style scoped>
+
+</style>
