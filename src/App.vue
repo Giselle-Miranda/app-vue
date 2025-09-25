de23a836adfc58600792c412256af6f4e38f0fee
Giselle <gi.miranda2424@gmail.com>
2025-09-25 11:32:00 -0300

    Edição do carrossel e titulo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
 import Carrossel from './components/Carrossel.vue'
+import Titulo from './components/Titulo.vue'
 
 </script>
 
 <template>
-
+     
     <router-view />
     <NavBar />
     <Carrossel />
+    <Titulo />
 
 </template>
 
---
+++ src/components/Titulo.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <div class="text-center" id="titulo">
+    <h1>Meu Portfólio</h1>
+    <h3>Desenvolvedor Front-End</h3>
+    <button type="button" class="btn btn-outline-secondary">Secondary</button>
+  </div>
+</template>
+
+<style>
+
+#titulo{
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
